5bc7dae18a96912f7416dae5b9884fd46df6714b
JoseM995 <josemendoza517@icloud.com>
2025-10-01 11:48:16 -0500

    Added new scrip
    Adde a new scrip for the healthbar

---
+++ Assets/Scripts/JoseMendoza/WeaponSwitching.cs
@@ -0,0 +1,91 @@
+using System;
+using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    public int selectWeapon = 0;
+    private int previousSelectedWeapon;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            int previousSelectedWeapon = selectWeapon;
+            
+            if (selectWeapon >= transform.childCount - 1)
+            {
+                selectWeapon = 0;
+                selectWeapon = 0;
+            }
+            else
+            {
+                selectWeapon++;
+            }
+
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectWeapon <= 0)
+            {
+                selectWeapon = transform.childCount - 1;
+                selectWeapon = 0;
+            }
+
+            else
+            {
+                selectWeapon--;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                selectWeapon = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >2)
+            {
+                selectWeapon = 1;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount > 3)
+            {
+                selectWeapon = 2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount > 4)
+            {
+                selectWeapon = 3;
+            }
+
+            if (previousSelectedWeapon != selectWeapon)
+            {
+                SelectedWeapon();             
+            }
+        }
+    }
+
+    private void SelectedWeapon()
+    {
+        throw new NotImplementedException();
+    }
+
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if (i == selectWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+}

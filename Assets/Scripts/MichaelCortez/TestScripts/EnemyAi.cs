b9c1efe80f82563b2cacd25117abae82f5b2ee55
killerck24 <165198469+killerck24@users.noreply.github.com>
2025-10-01 11:50:32 -0500

    worked on enemy AI

--- Assets/Scripts/MichaelCortez/TestScripts/EnemyAi.cs
+++ Assets/Scripts/MichaelCortez/TestScripts/EnemyAi.cs
@@ -12,17 +12,20 @@ public class EnemyAi : MonoBehaviour
 
     public LayerMask whatIsGroud, whatIsPlayer;
 
+    public float health;
+
     //Patroling
-        public Vector3 walkPoint;
+    public Vector3 walkPoint;
     bool walkPointSet;  
     public float walkPointRange;    
 
     //Attacking
         public float timeBetweenAttacks;
-        bool alreadyAttacked;   
+        bool alreadyAttacked;
+        public GameObject projectile;
 
     //States
-            public float sightRange, AttackRange;
+        public float sightRange, AttackRange;
         public bool playerInSightRange, playerInAttackRange;        
 
 
@@ -31,10 +34,9 @@ public class EnemyAi : MonoBehaviour
     {
         player = GameObject.Find("PlayerObj").transform;
         agent = GetComponent<NavMeshAgent>();
-    }   
+    }
 
-
-    private void Patroling()
+    private void Update()
     {
         //Check for sight and attack range  
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
@@ -44,12 +46,76 @@ public class EnemyAi : MonoBehaviour
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
 
+
+
+
+
+    }
+
+
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+
+        Vector2 distanceToWalkPoint = transform.position - walkPoint;
+
+        //WalkPoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGroud))
+            walkPointSet = true;
     }
 
     private void ChasePlayer()
     {
+        agent.SetDestination(player.position);
+
+        transform.LookAt(player);
+
+
+
+        if (!alreadyAttacked)
+        {
+
+            //Attack code here
+
+            //Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent < Rigidbody >
+            //rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+           // rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+
+
+
+            alreadyAttacked = true;
+
+
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            
+        }
+
+
 
     }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
 
     private void AttackPlayer()
     {
@@ -57,17 +123,10 @@ public class EnemyAi : MonoBehaviour
     }
 
 
-
-
-    // Start is called before the first frame update
-    void Start()
+    public void TakeDamage(int damage)
     {
-        
+        //health
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }

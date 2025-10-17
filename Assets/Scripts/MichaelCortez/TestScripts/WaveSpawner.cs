19a0e59b3183b7ae0dbd452797518c86fad6bca9
killerck24 <165198469+killerck24@users.noreply.github.com>
2025-10-17 11:49:57 -0500

    Started with the Wave Spawner Script

---
+++ Assets/Scripts/MichaelCortez/TestScripts/WaveSpawner.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    [SerializeField] private float countdown;
+
+    [SerializeField] private GameObject SpawnPoint;
+
+    public Wave[] waves;
+
+
+    private int currentWaveIndex = 0;
+
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            
+            countdown = waves[currentWaveIndex].timeToNextWave;
+            
+            StartCoroutine(SpawnWave());
+        }
+
+
+        if (waves[currentWaveIndex].enemiesLeft == 0)
+        {
+            currentWaveIndex++;
+        }
+
+
+
+    }
+
+
+
+
+
+    private IEnumerator SpawnWave()
+    {
+
+
+        for (int i = 0; i < waves[currentWaveIndex].enemies.Length; i++)
+        {
+
+            Enemy enemy = Instantiate(waves[currentWaveIndex].enemies[i], SpawnPoint.transform);
+
+            enemy.transform.SetParent(SpawnPoint.transform);
+
+            yield return new WaitForSeconds(waves[currentWaveIndex].timeToNextEnemy);
+        }
+
+
+
+
+    }
+
+
+
+
+
+    [System.Serializable]
+    public class Wave
+    {
+        public Enemy[] enemies;
+
+        public float timeToNextEnemy;
+        public float timeToNextWave;
+
+        [HideInInspector] public int enemiesLeft;
+    }
+
+
+
+
+    private void Start()
+    {
+        for (int i = 0;i < waves.Length; i++)
+        {
+            waves[i].enemiesLeft = waves[i].enemies.Length; 
+        }
+    }
+
+}
